c6ac5c9e3bc933f210c99d8019118e80a9928ffd
Mike Ryan <mike@adifferentengine.com>
2018-02-04 15:11:02 -0800

    Saving redis depdency for swamer

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 const config = require('config')
+const debug = require('debug')('swarmer')
+const dateformat = require('date-format')
 
 app.get('/', (req, res) => {
   res.send('Hi!')
+})
+const cache = require('express-redis-cache')({
+  host: config.get('db.redis.host'),
+  port: config.get('db.redis.port'),
+  expire: 60
+})
+cache.on('error', e => {
+  debug(`Cache error ${e.message}`)
+})
+cache.on('connected', e => {
+  debug(`Redis cache connected to ${config.get('db.redis.host')}:${config.get('db.redis.port')} ${e}`)
+})
+
+app.use('/cached', cache.route(), (req, res) => {
+  debug('Cache Miss')
+  res.send(dateformat('MM/dd/yy at hh:mm:ss'))
 })
 
 const server = app.listen(config.get('port'), () => {
   const host = server.address().address
   const port = server.address().port
-  console.log(`running at http://${host}:${port}' - environment is ${config.get('env')}`)
+  debug(`running at http://${host}:${port}' - environment is ${config.get('env')}`)
 })
