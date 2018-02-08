1d7ef3ed90ed7ad7d34921ff643eefb64b78e17c
Mike Ryan <mike@adifferentengine.com>
2018-02-08 09:08:58 -0800

    Saving base branch

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ const express = require('express')
 const app = express()
 const config = require('config')
 const debug = require('debug')('swarmer')
-const dateformat = require('date-format')
-const os = require('os')
 
 app.get('/', (req, res) => {
   res.send('Hi!')
 })
-const cache = require('express-redis-cache')({
-  host: config.get('db.redis.host'),
-  port: config.get('db.redis.port'),
-  expire: 60
-})
-cache.on('error', e => {
-  debug(`Cache error ${e.message}`)
-})
-cache.on('connected', e => {
-  debug(`Redis cache connected to ${config.get('db.redis.host')}:${config.get('db.redis.port')} ${e}`)
-})
-
-app.use('/cached', cache.route(), (req, res) => {
-  res.send(`<h1>Hi there!</h1>Date: ${dateformat('MM/dd/yy at hh:mm:ss')} <br />Hostname: ${os.hostname()}`)
+app.get('/healthcheck', (req, res) => {
+  res.send(200)
 })
 
 const server = app.listen(config.get('port'), () => {
